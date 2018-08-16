a505fb9fe8eda6146e8467b688b593dc218bd0ef
Maki 3 <f0wlf4ng@gmail.com>
2018-08-16 13:44:43 -0700

    Okay, turns out that I don't know how to react LOL

--- calendarexam/src/App.js
+++ calendarexam/src/App.js
@@ -8,27 +8,35 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import Radio from '@material-ui/core/Radio';
 import Paper from '@material-ui/core/Paper';
 
-function Year(props) {
-
-}
-function Month(props) {
-
-}
-function Days(props) {
-}
-
 class App extends Component {
+
   getTodaysDate() {
     var date = new Date()
-    return date.toLocaleDateString('us-EN')
+    var dateString = date.toLocaleDateString('us-EN')
+    var splitStringArray = str.split("/") // i.e Grab 8, 16, 2018.
+    return splitStringArray
   }
 
+  DaysInMonth(props) {
+   var daysInMonth = new Date({props.year}, {props.month}, 1).getDate()
+   return (
+     <h1>The days in the month are: {daysInMonth}</h1>
+   )
+  }
+  transformDate(props) {
+    return (
+
+    )
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React {this.getTodaysDate()}</h1>
-
+          <DaysInMonth
+            year={this.getTodaysDate()[0]}
+            month={this.getTodaysDate()[1]}
+          />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.

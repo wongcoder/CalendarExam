b3a98a454003c716580df4503ac401ad331c6b36
Maki 3 <f0wlf4ng@gmail.com>
2018-08-18 13:26:57 -0700

    Header is fully functional.

--- calendarexam/src/App.js
+++ calendarexam/src/App.js
@@ -6,7 +6,6 @@ import FormLabel from '@material-ui/core/FormLabel';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import Radio from '@material-ui/core/Radio';
-import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -14,23 +13,36 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import dateFns from 'date-fns';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
 
 class App extends Component {
+
   state = {
     currentMonth: new Date(), // Date() initializes it to the current date and time, intead of having to manually getTodaysDate().
     selectedDate: new Date()
   }
 
   // Event Handlers
-  previousMonth = () {
+  previousMonth = () => { // Equivalent to previousMonth(), just don't wanna write (), also tutorial I followed used this lOL
     this.setState({
       currentMonth: dateFns.subMonths(this.state.currentMonth, 1) // Provides an alternative instead of using minus minus, allowing me to use a date object.
     })
   }
 
-  nextMonth = () {
+  nextMonth = () => {
     this.setState({
       currentMonth: dateFns.addMonths(this.state.currentMonth, 1)
+    })
+  }
+
+  resetMonth = () => {
+    this.setState({
+      currentMonth: new Date() // Garbage collection if you dont' catch this I swear to f
     })
   }
 
@@ -41,7 +53,7 @@ class App extends Component {
       <div className="dateHeader">
         <Grid container spacing={16}>
           <Grid item>
-            <Button variant="contained" color="primary">Previous</Button>
+            <Button variant="contained" color="primary" onClick={this.previousMonth}>Previous</Button>
           </Grid>
           <Grid item>
             <Typography variant="headline" component="h2">
@@ -49,13 +61,15 @@ class App extends Component {
             </Typography>
           </Grid>
           <Grid item>
-            <Button variant="contained" color="primary">Next</Button>
+            <Button variant="contained" color="primary" onClick={this.nextMonth}>Next</Button>
           </Grid>
+          <Grid item>
+            <Button onClick={this.resetMonth}>Reset</Button>
         </Grid>
       </div>
-
     )
   }
+
   dateCard(props) {
     return (
       <Card className="Date">
@@ -71,9 +85,10 @@ class App extends Component {
     )
   }
 
-  generateGrid() {
+  generateGrid() { // generates all the little squares we need
     return(
-      <Grid table
+      <Grid table>
+      </Grid>
     )
   }
 
@@ -81,7 +96,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Grid container className={}
+        <Grid container className="something">
+          {this.renderHeader()}
+        </Grid>
       </div>
     );
   }
